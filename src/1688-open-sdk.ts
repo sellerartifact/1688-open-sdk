cd750763d83b2eb7555ee8335d2471bfef32d5b2
invalid w <wangjuesix@gmail.com>
2022-06-09 12:03:30 +0800

    fix: wrong params

--- src/1688-open-sdk.ts
+++ src/1688-open-sdk.ts
@@ -28,13 +28,13 @@ export default class ApiExecutor {
     }
   }
 
-  async refreshToken2AccessToken(){
+  async refreshToken2AccessToken(refreshToken: string){
     const url = `https://gw.open.1688.com/openapi/param2/1/system.oauth2/getToken/${this.appKey}`
     const params = {
         grant_type: "refresh_token",
         client_id: this.appKey,
         client_secret: this.appSecret,
-        refresh_token: this.refreshToken
+        refresh_token: refreshToken
     }
     const res = await axios({
       url,

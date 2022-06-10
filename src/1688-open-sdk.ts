a25aa1e8d7e04cb3407c581100b7ba8bfdf45d0f
invalid w <wangjuesix@gmail.com>
2022-06-10 10:15:26 +0800

    feat: 增加获取用户Token 以图搜图单元测试

--- src/1688-open-sdk.ts
+++ src/1688-open-sdk.ts
@@ -1,56 +1,44 @@
-import axios, { AxiosResponse } from "axios"
-import { UrlParam, computedAppSignature, GetTokenData } from "./lib/utils"
-
+import axios, { AxiosResponse } from 'axios'
+import { UrlParam, computedAppSignature, GetTokenData } from './lib/utils'
 
 export default class ApiExecutor {
+  constructor(private appKey: string, private appSecret: string) {}
 
-  constructor(private appKey: string, private appSecret: string){}
-
-  async execute(api: string, params: UrlParam, access_token: string){
+  async execute(api: string, params: UrlParam, access_token: string) {
     api = this.mergeApi(api)
     params.access_token = access_token
     params._aop_timestamp = Date.now()
     params._aop_signature = computedAppSignature(api, params, this.appSecret)
-
-    try{
-      const res = await axios({
-          url: this.mergeUrl(api),
-          method: "post",
-          params
-      })
-      return res.data
-    }catch(e){
-        const res =  (e as AxiosResponse)
-        if(res.data){
-            return res.data
-        }
-        throw new Error((e as Error).message)
-    }
+    const res = await axios({
+      url: this.mergeUrl(api),
+      method: 'post',
+      params
+    })
+    return res.data
   }
 
-  async refreshToken2AccessToken(refreshToken: string){
+  async refreshToken2AccessToken(refreshToken: string) {
     const url = `https://gw.open.1688.com/openapi/param2/1/system.oauth2/getToken/${this.appKey}`
     const params = {
-        grant_type: "refresh_token",
-        client_id: this.appKey,
-        client_secret: this.appSecret,
-        refresh_token: refreshToken
+      grant_type: 'refresh_token',
+      client_id: this.appKey,
+      client_secret: this.appSecret,
+      refresh_token: refreshToken
     }
     const res = await axios({
       url,
-      method: "post",
+      method: 'post',
       params
     })
     return res.data as GetTokenData
   }
 
-  private mergeApi(api: string){
+  private mergeApi(api: string) {
     return `param2/1/${api}/${this.appKey}`
   }
 
-  private mergeUrl(api: string){
+  private mergeUrl(api: string) {
     const request_url = 'http://gw.open.1688.com/openapi/'
     return request_url + api
   }
-
 }
--- test/1688-open-sdk.test.ts
+++ test/1688-open-sdk.test.ts
@@ -1,13 +1,36 @@
 import ApiExecutor from '../src/1688-open-sdk'
+import { GetTokenData } from '../src/lib/utils'
 /**
  * Dummy test
  */
+function createInstance() {
+  if (process.env.APPKEY && process.env.APPSECRET) {
+    const apiExecutor = new ApiExecutor(process.env.APPKEY, process.env.APPSECRET)
+    return apiExecutor
+  }
+}
+const apiExecutor = createInstance()
+let userData: GetTokenData | undefined
 describe('ApiExecutor test', () => {
   it('ApiExecutor is instantiable', () => {
-    console.log('process.env.APPSECRET', process.env)
-    if (process.env.APPKEY && process.env.APPSECRET) {
-      const apiExecutor = new ApiExecutor(process.env.APPKEY, process.env.APPSECRET)
-      expect(apiExecutor).toBeInstanceOf(ApiExecutor)
-    }
+    expect(apiExecutor).toBeInstanceOf(ApiExecutor)
+  })
+  it('refreshToken2AccessToken', async () => {
+    const refresh_token = '5a7eebfb-9573-4f5c-9f07-848535649e6c'
+    const data = await apiExecutor!.refreshToken2AccessToken(refresh_token)
+    userData = data
+    expect(data.access_token.length > 0).toBeTruthy()
+  })
+  it('execute method test', async () => {
+    const data = await apiExecutor!.execute(
+      'com.alibaba.linkplus/alibaba.cross.similar.offer.search',
+      {
+        picUrl:
+          'https://img.alicdn.com/imgextra/i1/O1CN01PwAoB21LuSuZE9Oqx_!!6000000001359-2-tps-1170-597.png',
+        page: 1
+      },
+      userData!.access_token
+    )
+    expect(data.result.result.length > 0).toBeTruthy()
   })
 })

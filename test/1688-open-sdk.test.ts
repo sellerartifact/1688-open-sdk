d81248b24391d7e761c69fe107775d915af75fa3
invalid w <wangjuesix@gmail.com>
2022-06-10 09:06:54 +0800

    Update 1688-open-sdk.test.ts

--- test/1688-open-sdk.test.ts
+++ test/1688-open-sdk.test.ts
@@ -2,7 +2,7 @@ import ApiExecutor from '../src/1688-open-sdk'
 /**
  * Dummy test
  */
-describe('Dummy test', () => {
+describe('ApiExecutor test', () => {
   it('ApiExecutor is instantiable', () => {
     console.log('process.env', process.env)
     expect(new ApiExecutor(process.env.APPKEY, process.env.APPSECRET)).toBeInstanceOf(ApiExecutor)
